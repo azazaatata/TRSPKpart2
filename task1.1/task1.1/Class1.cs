340ea6b19ae33d6eceef6628eb6dd5c2175f154b
azazaatata <tatata1108@mail.ru>
2021-09-15 13:06:37 +0300

    13:05
    Начало работы над заданием
    Создан класс для работы с массивом строк

---
+++ task1.1/task1.1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1._1
+{
+    class StringList
+    {
+        private string[] Str = new string[100];
+        public StringList()
+        {
+            Console.WriteLine("Массив строк создан");
+        }
+        public int Insert(string ins)
+        {
+
+        }
+    }
+}
---
+++ task1.1/task1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Куку привет");
+        }
+    }
+}
