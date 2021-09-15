2a377e9f163223f8b70da04c8914bccff2709ebd
azazaatata <tatata1108@mail.ru>
2021-09-15 15:40:50 +0300

    15:40
    Сделано задание 1.2

---
+++ task 1.2/task 1.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task_1._2
+{
+    class Math
+    {
+        public const double Pi = 3.14;//Константа, изменить ее нельзя
+        public const double e = 2.72;//Константа, изменить ее нельзя
+        public readonly int degree = 2;//Поле для чтения, значение задается при создании
+        public Math(int k)
+        {
+            degree = k;
+        }
+        public double Circle(double rad)
+        {
+            double S;
+            S = Pi * rad * rad;
+            return S;
+        }
+        public double ChangeCon(double ch)
+        {
+            //Pi = ch; - Невозможно
+            return Pi;
+        }
+        public int deg(int k)
+        {
+            int ret = 1;
+            for(int i = 0; i<k; k++)
+            {
+                ret *= k;
+            }
+            return ret;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите желаемое значение: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Math ex1 = new Math(k);
+            Console.WriteLine(ex1.degree);
+        }
+    }
+}
