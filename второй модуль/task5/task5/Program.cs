f0da1622f6605fad2f741a9e5626540e653c28e1
azazaatata <tatata1108@mail.ru>
2021-12-09 13:51:07 +0300

    Разделил на модули
    Сделал задание 5.1

---
+++ второй модуль/task5/task5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace task5
+{
+    class TrashForHeap
+    {
+        public int x;
+        public int y;
+        public int z;
+        public TrashForHeap(int X, int Y, int Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+        public void print()
+        {
+            Console.WriteLine("Координата х:{0}\nКоордината y:{1}\nКоордината z:{2}", x, y, z);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер до какого поколения хотите довести объект: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            k++;
+            if (k > 3)
+                k = 3;
+            
+
+            TrashForHeap trash;
+            Random rand = new Random();
+            int x, y, z;
+            x = rand.Next(0, 10);
+            y = rand.Next(0, 10);
+            z = rand.Next(0, 10);
+
+            trash = new TrashForHeap(x, y, z);
+
+            Console.WriteLine("Объект trash имеет координаты: ");
+            trash.print();
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+
+            for (int i = 0; i < k; i++)
+            {
+                int gen = GC.GetGeneration(trash);
+                Console.WriteLine("Поколение объекта: {0}", gen);
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+                GC.Collect();
+            }
+        }
+    }
+}
