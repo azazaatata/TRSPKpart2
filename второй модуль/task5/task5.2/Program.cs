6b906ef15eac9b570bd0b012ff5a69eb6e6e746f
azazaatata <tatata1108@mail.ru>
2021-12-09 16:05:28 +0300

    1
    1

--- второй модуль/task5/task5.2/Program.cs
+++ второй модуль/task5/task5.2/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace task5._2
 {
+    class Trash
+    {
+        static public bool[] oneByte;
+        public Trash(int k)
+        {
+            oneByte = new bool[k];
+        }
+    }
     internal class Program
     {
+        static void ChooseSize()
+        {
+            while(true)
+            {
+                Console.WriteLine("Введите размер в байтах для создаваемой переменной(если хотите выйти введите 0) : ");
+                int count = Convert.ToInt32(Console.ReadLine());
+                if(count == 0)
+                    break;
+
+                Trash check = new Trash(count);
+                int gen = GC.GetGeneration(Trash.oneByte);
+                Console.WriteLine("Поколение созданной переменной: ");
+            }
+        }
+        static void BigTrash()
+        {
+            int k = 6000;
+            decimal[] m = new decimal[k];
+
+            Console.WriteLine("Сколько сборок мусора провести?");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int gen = GC.GetGeneration(m);
+            Console.WriteLine("Поколение объекта до сборки мусора: {0}", gen);
+
+            for (int i = 0; i < count; i++)
+            {
+                gen = GC.GetGeneration(m);
+                Console.WriteLine("Поколение после {0} сборки мусора: {1}", i + 1, gen);
+            }
+        }
         static void Main(string[] args)
         {
-            
+            BigTrash();
         }
     }
 }
--- второй модуль/task5/task5/Program.cs
+++ второй модуль/task5/task5/Program.cs
@@ -1,56 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace task5
 {
-    class TrashForHeap
+    class Test
     {
-        public int x;
-        public int y;
-        public int z;
-        public TrashForHeap(int X, int Y, int Z)
+        public int k;
+        public Test()
         {
-            x = X;
-            y = Y;
-            z = Z;
-        }
-        public void print()
-        {
-            Console.WriteLine("Координата х:{0}\nКоордината y:{1}\nКоордината z:{2}", x, y, z);
+            Random rnd = new Random();
+            k = rnd.Next(0, 10);
         }
     }
     internal class Program
     {
-        static void Main(string[] args)
+        static void Punkt1()
         {
-            Console.WriteLine("Введите номер до какого поколения хотите довести объект(максимальное поколение 2): ");
+            Console.WriteLine("Введите номер до какого поколения хотите довести переменную(максимальное поколение 2): ");
             int k = Convert.ToInt32(Console.ReadLine());
             k++;
-            if (k > 3)
-                k = 3;
-            
+            if (k > 2)
+                k = 2;
+            Test f = new Test();
+            int gen = GC.GetGeneration(f);
+            Console.WriteLine("Поколение до чистки мусора: {0}", gen);
 
-            TrashForHeap trash;
-            Random rand = new Random();
-            int x, y, z;
-            x = rand.Next(0, 10);
-            y = rand.Next(0, 10);
-            z = rand.Next(0, 10);
-
-            trash = new TrashForHeap(x, y, z);
-
-            Console.WriteLine("Объект trash имеет координаты: ");
-            trash.print();
             System.Threading.Thread.Sleep(1000);
             Console.Clear();
 
             for (int i = 0; i < k; i++)
             {
-                int gen = GC.GetGeneration(trash);
+                GC.Collect();
+                gen = GC.GetGeneration(f);
                 Console.WriteLine("Поколение объекта: {0}", gen);
                 Console.WriteLine();
                 System.Threading.Thread.Sleep(500);
-                Console.Clear();
+                if (i < k - 1)
+                    Console.Clear();
+            }
+        }
+
+        static void Punkt2()
+        {
+            Console.WriteLine("Введите количество проходов цикла: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Test t1 = new Test();
+            for (int i = 0; i < count; i++)
+            {
+                int gen = GC.GetGeneration(t1);
+                Console.WriteLine("Поколение переменной созданой до цикла: {0}", gen);
+                Test t2 = new Test();
+                gen = GC.GetGeneration(t2);
+                Console.WriteLine("Поколение переменной созданой в цикле: {0}", gen);
                 GC.Collect();
+                System.Threading.Thread.Sleep(400);
+                Console.Clear();
+            }
+        }
+
+        static void Punkt3()
+        {
+            Console.WriteLine("Введите количество проходов цикла: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Test t1 = new Test();
+            List<Test> m = new List<Test>();
+            for (int i = 0; i < count; i++)
+            {
+                int gen = GC.GetGeneration(t1);
+                Console.WriteLine("Поколение переменной созданой до цикла: {0}", gen);
+                Console.WriteLine();
+
+                Test t2 = new Test();
+                gen = GC.GetGeneration(t2);
+                Console.WriteLine("Поколение переменной созданой в цикле: {0}", gen);
+                Console.WriteLine();
+                m.Add(t2);
+
+
+                for (int j = 0; j < m.Count - 1; j++)
+                {
+                    gen = GC.GetGeneration(m[j]);
+                    Console.WriteLine("{0} переменная скопированная в список имеет поколение :{1}", j + 1, gen);
+                }
+                GC.Collect();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какой из абзацев выполнить?");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            if(choose == 1)
+            {
+                Punkt1();
+            }
+            else if(choose == 2)
+            {
+                Punkt2();
+            }
+            else
+            {
+                Punkt3();
             }
         }
     }
